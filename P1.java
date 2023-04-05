2b9c10b34b6f612329974601472403e713db0de6
Dali <82356573+OMateusSO@users.noreply.github.com>
2023-04-05 19:36:42 -0300

    teste commit
    teste

---
+++ P1.java
@@ -0,0 +1,11 @@
+
+import javax.swing.JFrame;
+
+public class P1 {
+
+    public static void main(String[] args) {
+        JFrame janela = new JFrame("Programa");
+        janela.setSize(600, 600);
+        janela.setVisible(true);
+    }
+}
